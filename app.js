986ae1ffe37f8cb980ed61f3485226f885ee9f0b
Kirill Lyashenko <tltelf@yandex.ru>
2023-11-09 14:49:18 +0400

    Tuples

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
 "use strict";
-const skills = ['Dev', 'DevOps', 'Testing'];
-for (const skill of skills) {
-    console.log(skill.toLowerCase());
-}
-const res = skills
-    .filter((s) => s !== 'DevOps')
-    .map((s) => {
-    return 1;
-})
-    .reduce((a, b) => a + b);
-console.log(res);
+const skill = [1, 'Dev'];
+const [id, skillName] = skill;
+const arr = [1, 'sdf', true, true, false];
