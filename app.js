d13f81fc480c88aba3f88ca82756285554d4270b
Kirill Lyashenko <tltelf@yandex.ru>
2023-11-14 22:56:20 +0400

    Never

--- app.js
+++ app.js
@@ -1,45 +1,35 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-let input;
-input = 3;
-input = ['sf', 'sdf'];
-function run(i) {
-    if (typeof i == 'number') {
-        i++;
-    }
-    else {
-        i;
+function genereateError(message) {
+    throw new Error(message);
+}
+function dumpError() {
+    while (true) { }
+}
+function rec() {
+    return rec();
+}
+function processAction(action) {
+    switch (action) {
+        case 'refund':
+            //...
+            break;
+        case 'checkout':
+            // ...
+            break;
+        case 'reject':
+            // ...
+            break;
+        default:
+            const _ = action;
+            throw new Error('Нет такого action');
     }
 }
-run(input);
-function getData() {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            yield fetch('');
-        }
-        catch (error) {
-            if (error instanceof Error) {
-                console.log(error.message);
-            }
-        }
-    });
-}
-function getDataForce() {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            yield fetch('');
-        }
-        catch (error) {
-            const e = error;
-            console.log(e.message);
-        }
-    });
+function isString(x) {
+    if (typeof x === 'string') {
+        return true;
+    }
+    else if (typeof x === 'number') {
+        return false;
+    }
+    genereateError('sdfsdf');
 }
