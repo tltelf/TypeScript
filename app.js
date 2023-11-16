cd578d8e8104fac1a6dcfe7b34770c14aaca4519
Kirill Lyashenko <tltelf@yandex.ru>
2023-11-16 23:15:22 +0400

    Приведение типов

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 "use strict";
-const n = null;
-const n1 = null;
-const n2 = null;
-const n3 = null;
-const n4 = null;
-const n5 = null;
-function getUser() {
-    if (Math.random() > 0.5) {
-        return null;
-    }
-    else {
-        return {
-            name: 'Вася'
-        };
-    }
-}
-const user = getUser();
-if (user) {
-    const n55 = user.name;
+let a = 5;
+let b = a.toString();
+let e = new String(a).valueOf();
+let f = new Boolean(a).valueOf();
+let c = 'sdfs';
+let d = parseInt(c);
+const user = {
+    name: 'Вася',
+    email: 'vasiliy@gmail.com',
+    login: 'Vasya',
+};
+const admin = Object.assign(Object.assign({}, user), { role: 1 });
+function userToAdmin(user) {
+    return {
+        name: user.name,
+        role: 1,
+    };
 }
