a9d9267b3388343633d44a6ded3e27f70a7529a7
Kirill Lyashenko <tltelf@yandex.ru>
2023-11-17 23:05:31 +0400

    Type Guard

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 "use strict";
-let a = 5;
-let b = a.toString();
-let e = new String(a).valueOf();
-let f = new Boolean(a).valueOf();
-let c = 'sdfs';
-let d = parseInt(c);
 const user = {
     name: 'Вася',
     email: 'vasiliy@gmail.com',
     login: 'Vasya',
 };
-const admin = Object.assign(Object.assign({}, user), { role: 1 });
-function userToAdmin(user) {
-    return {
-        name: user.name,
-        role: 1,
-    };
+function logId(id) {
+    if (isString(id)) {
+        console.log(id);
+    }
+    else {
+        console.log(id);
+    }
+}
+function isString(x) {
+    return typeof x === 'string';
+}
+function isAdmin(user) {
+    return 'role' in user;
+}
+function isAdminAlternative(user) {
+    return user.role !== undefined;
+}
+function setRoleZero(user) {
+    if (isAdmin(user)) {
+        user.role = 0;
+    }
+    else {
+        throw new Error('Пользователь не админ');
+    }
 }
