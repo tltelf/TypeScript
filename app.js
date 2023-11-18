89039302e32af8b42154ec21a0cf22cb89ab20aa
Kirill Lyashenko <tltelf@yandex.ru>
2023-11-18 23:42:41 +0400

    Создание класса

--- app.js
+++ app.js
@@ -1,31 +1,14 @@
 "use strict";
-const user = {
-    name: 'Вася',
-    email: 'vasiliy@gmail.com',
-    login: 'Vasya',
-};
-function logId(id) {
-    if (isString(id)) {
-        console.log(id);
-    }
-    else {
-        console.log(id);
+class User {
+    constructor(name) {
+        this.name = name;
     }
 }
-function isString(x) {
-    return typeof x === 'string';
+const user = new User('Вася');
+console.log(user);
+user.name = 'Петя';
+console.log(user);
+class Admin {
 }
-function isAdmin(user) {
-    return 'role' in user;
-}
-function isAdminAlternative(user) {
-    return user.role !== undefined;
-}
-function setRoleZero(user) {
-    if (isAdmin(user)) {
-        user.role = 0;
-    }
-    else {
-        throw new Error('Пользователь не админ');
-    }
-}
+const admin = new Admin();
+admin.role = 1;
