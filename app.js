7c8872cc126de89fc530281c4296c0f34c4f1ba8
Kirill Lyashenko <tltelf@yandex.ru>
2023-11-19 20:02:04 +0400

    Конструктор

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 "use strict";
 class User {
-    constructor(name) {
-        this.name = name;
+    constructor(ageOrName, age) {
+        if (typeof ageOrName === 'string') {
+            this.name = ageOrName;
+        }
+        else if (typeof ageOrName === 'number') {
+            this.age = ageOrName;
+        }
+        if (typeof age === 'number') {
+            this.age = age;
+        }
     }
 }
 const user = new User('Вася');
-console.log(user);
-user.name = 'Петя';
-console.log(user);
-class Admin {
-}
-const admin = new Admin();
-admin.role = 1;
+const user2 = new User();
+const user3 = new User(33);
+const user4 = new User('Вася', 33);
