e7ca3a0904cc3de215ce51a0d1e4313d6957e136
Kirill Lyashenko <tltelf@yandex.ru>
2023-11-23 22:50:17 +0400

    Extends

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-class Looger {
-    log(...args) {
-        console.log(...args);
+class Payment {
+    constructor(id) {
+        this.status = 'new';
+        this.id = id;
     }
-    error(...args) {
-        return __awaiter(this, void 0, void 0, function* () {
-            // Кинуть во внешнюю систему
-            console.log(...args);
-        });
+    pay() {
+        this.status = 'paid';
     }
 }
-class User {
-    pay(paymentId) {
-        ///
+class PersistedPayment extends Payment {
+    constructor() {
+        const id = Math.random();
+        super(id);
+    }
+    save() {
+        // Сохраняет в базу
+    }
+    pay(date) {
+        super.pay();
+        if (date) {
+            this.paidAt = date;
+        }
     }
 }
+new PersistedPayment();
