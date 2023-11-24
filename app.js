92a94af6c28f9c39f4d7143ead4060b558bfe3d8
Kirill Lyashenko <tltelf@yandex.ru>
2023-11-24 20:37:26 +0400

    Особенности наследования

--- app.js
+++ app.js
@@ -24,3 +24,24 @@ class PersistedPayment extends Payment {
     }
 }
 new PersistedPayment();
+class User {
+    constructor() {
+        this.name = 'user';
+        console.log(this.name);
+    }
+}
+class Admin extends User {
+    constructor() {
+        super();
+        this.name = 'admin';
+        console.log(this.name);
+    }
+}
+new Admin();
+// new Error('');
+class HttpError extends Error {
+    constructor(message, code) {
+        super(message);
+        this.code = code !== null && code !== void 0 ? code : 500;
+    }
+}
